f07b3236242158c9f3cc47aa43c2e34f1e045a7a
Pierre <pierre.de.vreyer@gmail.com>
2020-05-07 11:52:36 +0800

    Add a stub image gallery

---
+++ js/gallery.js
@@ -0,0 +1,7 @@
+function activateGallery() {
+  if (document.querySelector("h1")) {
+    alert(document.querySelector("h1").textContent);
+  } else {
+    alert("Cowabunga Dude!".toUpperCase());
+  }
+}
